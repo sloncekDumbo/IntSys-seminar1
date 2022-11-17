08154916591a944b24a2c5994fc1274a7913ef81
Janez Tomšič <janez2001@gmail.com>
2022-11-17 18:00:49 +0100

    Basic fitness function and default GA settings

--- main.R
+++ main.R
@@ -10,77 +10,157 @@ maze_to_matrix <- function( maze )
 {
   nrow <- length( maze )
   ncol <- nchar( maze[1] )
-  maze_flat <- unlist( strsplit( maze1, '' ) )
-  mat <- matrix( maze_flat, nrow, ncol, byrow=TRUE )
+  maze_flat <- unlist( strsplit( maze, '' ) )
+  mat <- matrix( maze_flat, nrow, ncol, byrow = TRUE )
   return( mat )
 }
 
 # Read maze
-maze <- maze_to_matrix( maze1 )
-max_path_length <- nrow( maze ) * ncol( maze )
+maze <- maze_to_matrix( maze2 )
 start <- which( maze == 'S', arr.ind = TRUE )[1,]
+end <- which( maze == 'E', arr.ind = TRUE )[1,]
+max_path_length <- nrow( maze ) * ncol( maze )
 
-# Generates an initial population of subjects that move randomly
-init_population <- function( object )
+# Decodes path encoded with numbers
+decode_path <- function( path )
 {
-  subj <- gareal_Population( object )
-  subj <- matrix( as.integer(subj), dim(subj) )
-  return(subj)
-}
-
-fitness <- function( path )
-{
+  res <- c()
+  found_path <- FALSE
+  hit_wall <- FALSE
   pos <- start
-  nrow <- dim( maze ) [1]
-  ncol <- dim( maze ) [2]
-  path_map <- matrix(FALSE, nrow, ncol)
-  path_map[ pos[['row']],  pos[['col']] ] <- TRUE
+  path <- as.integer( path )
   
   for (move in path) {
     
-    # TODO:
-    #  1) Check if current position is wall
-    #  2) Check if moving in circles?
-    
-    if (move == 1)
-    {
+    # Make a move
+    if (move == 1) {    # Right
+      res <- c(res, "R")
       pos['col'] <- pos['col'] + 1
-      
+    }
+    else if (move == 2) # Up
+    {
+      res <- c(res, "U")
+      pos['row'] <- pos['row'] - 1
+    }
+    else if (move == 3) # Left
+    {
+      res <- c(res, "L")
+      pos['col'] <- pos['col'] - 1
+    }
+    else if (move == 4) # Down
+    {
+      res <- c(res, "D")
+      pos['row'] <- pos['row'] + 1
+    }
+    
+    # Check current position
+    curr <- '#'  # Wall by default - for out of bounds cases
+    if (1 <= pos['col'] && pos['col'] <= ncol(maze) && 1 <= pos['row'] && pos['row'] <= nrow(maze)) {
+      curr <- maze[pos['row'], pos['col']]
+    }
+    
+    if (curr == '#') {
+      hit_wall <- TRUE
+      break
+    }
+    else if (curr == 'E') {
+      found_path <- TRUE
+      break
+    }
+    
+  }
+  
+  ret <- list( "found" = found_path, "wall" = hit_wall, "path" = res )
+  return( ret )
+}
+
+# Fitness function
+fitness <- function( path )
+{
+  path = as.integer( path )
+  pos <- start
+  curr <- 'S'
+  path_len <- 0
+  walls_hit <- 0
+  
+  for (move in path) {
+    
+    path_len <- path_len + 1
+    
+    # Make a move
+    if (move == 1) {
+      pos['col'] <- pos['col'] + 1
     }
     else if (move == 2)
     {
       pos['row'] <- pos['row'] - 1
-      
     }
     else if (move == 3)
     {
       pos['col'] <- pos['col'] - 1
-      
     }
     else if (move == 4)
     {
       pos['row'] <- pos['row'] + 1
     }
     
-    if (0 <= pos[['col']] && pos[['col']] <= ncol && 0 <= pos[['row']] && pos[['row']] <= nrow)
+    # Check current position
+    curr <- '#'  # Wall by default - for out of bounds cases
+    if (1 <= pos['col'] && pos['col'] <= ncol(maze) && 1 <= pos['row'] && pos['row'] <= nrow(maze))
     {
-      path_map[ pos[['row']],  pos[['col']] ] <- TRUE
+      curr <- maze[pos['row'], pos['col']]
     }
-    else
-      return(NULL)
+    
+    # Reached the end
+    if (curr == 'E') {
+      break
+    
+    # Hit a wall
+    } else if (curr == '#') {
+      walls_hit <- walls_hit + 1
+    }
   }
   
+  # Compute fitness score:
+  # a) The path goes through walls - fitness based on number of walls hit
+  if (walls_hit > 0) {
+    return( -1 * max_path_length * (ncol(maze) + nrow(maze)) * walls_hit )
+    
+  # b) End was not reached - fitness based on distance to end
+  } else if (curr != 'E') {
+    dist <- sum( abs( pos - end ) )
+    return( -1 * max_path_length * dist )
   
-  
-  return(path_map)
+  # c) End was reached - fitness based on path length
+  } else {
+    return( -1 * path_len )
+  }
   
 }
 
+# Run the genetic algorithm
+GA <- ga(type = "real-valued",
+         fitness = fitness,
+         lower = rep(1, max_path_length),
+         upper = rep(5, max_path_length),
+         popSize = 100,
+         maxiter = 1000,
+         elitism = 1)
 
-# Path encoding:
-#  1 - right
-#  2 - up
-#  3 - left
-#  4 - down
-GA <- ga(type = "real-valued", fitness = fitness, lower = rep(1, max_path_length), upper = rep(5, max_path_length), population = init_population, popSize = 10, maxiter=10, pmutation = 0, pcrossover = 0)
+plot( GA )
+path_enc <- GA@solution[1,]
+result <- decode_path( path_enc )
+
+if (result$found) {
+  print( "Found path :D" )
+  print( result$path )
+  
+} else if (result$wall) {
+  print( "Hit a wall :'( My best attempt is:" )
+  print( result$path )
+  
+} else {
+  print( "Path not found :'( My best attempt is:" )
+  print( result$path )
+}
 
